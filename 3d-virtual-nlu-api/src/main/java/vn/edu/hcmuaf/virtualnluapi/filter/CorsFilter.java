b3b8b8006171bb5c12621471aa4ba523a6a80c43
nnquy <nnquy2k3@gmail.com>
2025-03-24 13:42:36 +0700

    config(CORS): add filter config cors

---
+++ 3d-virtual-nlu-api/src/main/java/vn/edu/hcmuaf/virtualnluapi/filter/CorsFilter.java
@@ -0,0 +1,39 @@
+package vn.edu.hcmuaf.virtualnluapi.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*") // Áp dụng cho tất cả API
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        res.setHeader("Access-Control-Allow-Origin", "*"); // Cho phép tất cả domain
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Xử lý request OPTIONS (preflight request)
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
+}
