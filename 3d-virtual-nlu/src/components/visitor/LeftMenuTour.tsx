0b514c6a5812f8916a922ce71135a9be20300c92
nnquy <nnquy2k3@gmail.com>
2025-04-26 21:24:34 +0700

    feat(show-master-node-ui & API): separate component left menu to show master node for vistor and add API for it

---
+++ 3d-virtual-nlu/src/components/visitor/LeftMenuTour.tsx
@@ -0,0 +1,59 @@
+import { useDispatch, useSelector } from "react-redux";
+import styles from "../../styles/leftMenuTour.module.css";
+import { FaSearch } from "react-icons/fa";
+import { AppDispatch, RootState } from "../../redux/Store";
+import { fetchMasterNodes } from "../../redux/slices/DataSlice";
+import { useEffect, useState } from "react";
+
+interface LeftMenuProps {
+  isMenuVisible: boolean;
+}
+
+const LeftMenuTour = ({ isMenuVisible }: LeftMenuProps) => {
+  {
+    {
+      /* Menu bên trái */
+    }
+  }
+  const dispatch = useDispatch<AppDispatch>();
+  const listMasterNode = useSelector(
+    (state: RootState) => state.data.masterNodes
+  );
+  console.log("listMasterNode", listMasterNode);
+  useEffect(() => {
+    dispatch(fetchMasterNodes());
+  }, [dispatch]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredNodes = listMasterNode.filter((node) =>
+    node.spaceName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className={`${styles.left_menu} ${isMenuVisible ? styles.show : ""}`}>
+      <div className={styles.header}>
+        <h2>NLU Tour</h2>
+        <div className={styles.searchBox}>
+          <input type="text" className={styles.inputSeach} placeholder="Tên không gian.." 
+            onChange={(e) => setSearchTerm(e.target.value)}/>
+          <FaSearch className={styles.searchBtn} />
+        </div>
+      </div>
+      <ul>
+        {filteredNodes.map((node) => (
+          <li
+            key={node.id}
+            className={styles.node}
+            style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${node.url})`,
+            }}
+          >
+            <span className={styles.nodeName}>{node.spaceName}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LeftMenuTour;
